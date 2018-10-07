cd77768b4a48ff304c7b8faf7f74762c65068f75
Joonas Loppi <joonas@joonas.fi>
2018-10-07 13:59:09 +0300

    Improve Stopper: more safety, less buffering, "Stopped" flag

--- stopper/stopper.go
+++ stopper/stopper.go
@@ -8,44 +8,48 @@ import (
 // to signal that they're stopped cleanly.
 // does about same as https://stackoverflow.com/a/20166250
 type Manager struct {
-	workerStopSignal chan bool
-	workers          int
+	workers          []*Stopper
 	workersWaitGroup *sync.WaitGroup
 }
 
 func NewManager() *Manager {
 	return &Manager{
-		workerStopSignal: make(chan bool, 16),
-		workers:          0,
+		workers:          []*Stopper{},
 		workersWaitGroup: &sync.WaitGroup{},
 	}
 }
 
-func (s *Manager) StopAllWorkersAndWait() {
-	// fire as many stop messages on the stop signal channel as there are stoppers (workers)
-	for i := 0; i < s.workers; i++ {
-		s.workerStopSignal <- true
+func (m *Manager) StopAllWorkersAndWait() {
+	for _, worker := range m.workers {
+		worker.Stopped = true
+		worker.ShouldStop <- true
 	}
 
-	s.workersWaitGroup.Wait()
+	m.workersWaitGroup.Wait()
 }
 
 // creates new Stopper, which is the worker's API for getting a stop signal and reporting finish
-func (s *Manager) Stopper() *Stopper {
-	s.workers++
-	s.workersWaitGroup.Add(1)
+func (m *Manager) Stopper() *Stopper {
+	m.workersWaitGroup.Add(1)
 
-	return &Stopper{
-		ShouldStop:       s.workerStopSignal,
-		workersWaitGroup: s.workersWaitGroup,
+	stopper := &Stopper{
+		Stopped:          false,
+		ShouldStop:       make(chan bool, 1), // buffered so worker recv waiting not required
+		workersWaitGroup: m.workersWaitGroup,
 	}
+
+	m.workers = append(m.workers, stopper)
+
+	return stopper
 }
 
 type Stopper struct {
+	Stopped          bool
 	ShouldStop       chan bool // each worker must read exactly one ShouldStop signal
 	workersWaitGroup *sync.WaitGroup
 }
 
 func (s *Stopper) Done() {
 	s.workersWaitGroup.Done()
+	s.workersWaitGroup = nil // prevent calling Done() twice
 }
