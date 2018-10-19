1716cd54fdb236b6d823820a9cee9fa69b0b1ab1
Joonas Loppi <joonas@joonas.fi>
2018-10-19 13:11:04 +0300

    bidipipe: simplify code, add tests, fix serious bug (error could be undetected)

--- bidipipe/bidipipe.go
+++ bidipipe/bidipipe.go
@@ -9,54 +9,13 @@ import (
 func Pipe(party1 io.ReadWriteCloser, party1Name string, party2 io.ReadWriteCloser, party2Name string) error {
 	allIoFinished := &sync.WaitGroup{}
 
-	// we have two goroutines Copy() to different directions. either side could encounter
-	// the error either from its read (source) or write (destination) side, so irregardless
-	// of direction we should try to close both ends
-	closeBothEnds := func() {
-		party1.Close()
-		party2.Close()
-	}
-
 	// take note of the first error to happen when Copy()ing to both directions. since when
 	// encountering error, we close both ends and that Close() will cause other (expected) error
-	// so we are not interested in that. "first error" can be nil, which means clean EOF
+	// so we are not interested in that.
 	firstErrorCh := make(chan error, 2)
 
-	go func(done *sync.WaitGroup) {
-		defer done.Done()
-
-		_, errCopyToParty1 := io.Copy(party1, party2)
-
-		if errCopyToParty1 != nil {
-			firstErrorCh <- fmt.Errorf(
-				"bidipipe: %s -> %s error: %s",
-				party2Name,
-				party1Name,
-				errCopyToParty1.Error())
-		} else { // clean EOF
-			firstErrorCh <- nil
-		}
-
-		closeBothEnds()
-	}(waiterReference(allIoFinished))
-
-	go func(done *sync.WaitGroup) {
-		defer done.Done()
-
-		_, errCopyToParty2 := io.Copy(party2, party1)
-
-		if errCopyToParty2 != nil {
-			firstErrorCh <- fmt.Errorf(
-				"bidipipe: %s -> %s error: %s",
-				party1Name,
-				party2Name,
-				errCopyToParty2.Error())
-		} else {
-			firstErrorCh <- nil
-		}
-
-		closeBothEnds()
-	}(waiterReference(allIoFinished))
+	go pipeOneDir(party1, party1Name, party2, party2Name, waiterAdd(allIoFinished), firstErrorCh)
+	go pipeOneDir(party2, party2Name, party1, party1Name, waiterAdd(allIoFinished), firstErrorCh)
 
 	allIoFinished.Wait()
 
@@ -64,11 +23,31 @@ func Pipe(party1 io.ReadWriteCloser, party1Name string, party2 io.ReadWriteClose
 	case firstError := <-firstErrorCh:
 		return firstError
 	default:
-		return nil // no error - this probably shouldnt happen
+		return nil
 	}
 }
 
-func waiterReference(wg *sync.WaitGroup) *sync.WaitGroup {
+func pipeOneDir(dst io.ReadWriteCloser, dstName string, src io.ReadWriteCloser, srcName string, done *sync.WaitGroup, firstErrorCh chan error) {
+	defer done.Done()
+
+	_, errCopyDst := io.Copy(dst, src)
+
+	if errCopyDst != nil {
+		firstErrorCh <- fmt.Errorf(
+			"bidipipe: %s -> %s error: %s",
+			srcName,
+			dstName,
+			errCopyDst.Error())
+	}
+
+	// we have two goroutines Copy() to different directions. either goroutine could encounter
+	// the error either from its read (source) or write (destination) side, so irregardless
+	// of direction we should try to close both ends
+	src.Close()
+	dst.Close()
+}
+
+func waiterAdd(wg *sync.WaitGroup) *sync.WaitGroup {
 	wg.Add(1)
 	return wg
 }
---
+++ bidipipe/bidipipe_test.go
@@ -0,0 +1,73 @@
+package bidipipe
+
+import (
+	"errors"
+	"github.com/function61/gokit/assert"
+	"io"
+	"testing"
+)
+
+func TestPipe(t *testing.T) {
+	person1 := &ReaderWriterMock{writes: "Nice to meet you, Person2"}
+	person2 := &ReaderWriterMock{writes: "Thanks Person1, pleasure is all mine"}
+
+	err := Pipe(person1, "person1", person2, "person2")
+
+	assert.True(t, err == nil)
+	assert.EqualString(t, person2.received, "Nice to meet you, Person2")
+	assert.EqualString(t, person1.received, "Thanks Person1, pleasure is all mine")
+}
+
+func TestPipeWithErrorFromPerson1(t *testing.T) {
+	person1 := &ReaderWriterMock{writes: "Nice to meet you, Person2", readsWillError: true}
+	person2 := &ReaderWriterMock{writes: "Thanks Person1, pleasure is all mine"}
+
+	err := Pipe(person1, "person1", person2, "person2")
+
+	assert.EqualString(t, err.Error(), "bidipipe: person1 -> person2 error: hurr durr, I broke")
+	assert.EqualString(t, person2.received, "")
+	assert.EqualString(t, person1.received, "Thanks Person1, pleasure is all mine")
+}
+
+func TestPipeWithErrorFromPerson2(t *testing.T) {
+	person1 := &ReaderWriterMock{writes: "Nice to meet you, Person2"}
+	person2 := &ReaderWriterMock{writes: "Thanks Person1, pleasure is all mine", readsWillError: true}
+
+	err := Pipe(person1, "person1", person2, "person2")
+
+	assert.EqualString(t, err.Error(), "bidipipe: person2 -> person1 error: hurr durr, I broke")
+	assert.EqualString(t, person2.received, "Nice to meet you, Person2")
+	assert.EqualString(t, person1.received, "")
+}
+
+type ReaderWriterMock struct {
+	writes         string
+	received       string
+	readAlready    bool
+	readsWillError bool
+}
+
+// implement io.ReadWriteCloser
+
+func (m *ReaderWriterMock) Read(p []byte) (int, error) {
+	if m.readsWillError {
+		return 0, errors.New("hurr durr, I broke")
+	}
+
+	if m.readAlready {
+		return 0, io.EOF
+	}
+
+	copy(p[:], m.writes)
+	m.readAlready = true
+	return len(m.writes), nil
+}
+
+func (m *ReaderWriterMock) Write(p []byte) (int, error) {
+	m.received = string(p)
+	return len(p), nil
+}
+
+func (m *ReaderWriterMock) Close() error {
+	return nil
+}
