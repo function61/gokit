7ebb89b12d11832f207f64673a1fe8fff4f61362
Joonas Loppi <joonas@joonas.fi>
2018-11-01 16:07:10 +0200

    ossignal: InterruptOrTerminate() should be a buffered channel

--- ossignal/ossignal.go
+++ ossignal/ossignal.go
@@ -12,7 +12,8 @@ import (
 //     https://en.wikipedia.org/wiki/Signal_(IPC)
 
 func InterruptOrTerminate() <-chan os.Signal {
-	ch := make(chan os.Signal)
+	// need a buffered channel so a nonblocking select will catch this
+	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	return ch
 }
