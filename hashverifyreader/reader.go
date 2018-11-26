15789f4b4b3116da9d3c274137b25b7ad45f3867
Joonas Loppi <joonas@joonas.fi>
2018-11-26 13:43:49 +0200

    new package: hashverifyreader

---
+++ hashverifyreader/reader.go
@@ -0,0 +1,70 @@
+package hashverifyreader
+
+import (
+	"bytes"
+	"errors"
+	"hash"
+	"io"
+)
+
+/*
+    +--------+
+    |        |
+    | Source |
+    |        |
+    +---+----+
+        |
+        |               +------+
+      +-v---+           |      |
+      |     +-----------> Hash |
+      | Tee |           |      |
+      |     |           +---+--+
+      +--+--+               |
+         |           +------v-----+
+         |           |            |
+         |    +------+ HashVerify |
+1. until |    |      |            |
+ tee EOF |    |      +------------+
+         |    | 2. after tee
+      +--v----v-----+ EOF
+      |             |
+      | MultiReader |
+      |             |
+      +------+------+
+             |
+             |
+        +----v-----+
+        |          |
+        | Consumer |
+        |          |
+        +----------+
+*/
+
+// creates a two-part reader whose first part is connected to source, and the second part
+// (= when source read to EOF) Read() (on hashVerifyReader) immediately returns EOF if hash matches
+func New(source io.Reader, hash hash.Hash, expectedHash []byte) io.Reader {
+	// create a T-junction to pump the read bytes 1:1 to hash fn
+	sourceTee := io.TeeReader(source, hash)
+
+	// when we get EOF from the Tee, last write is guaranteed to be pumped to the hash.
+	// MultiReader is read sequentially so when Read() is finally called on our
+	// hashVerifyReader we can safely access the finished digest and can return error to
+	// the consumer if the source data was corrupted
+	return io.MultiReader(sourceTee, &hashVerifyReader{
+		expectedHash: expectedHash,
+		hash:         hash,
+	})
+}
+
+type hashVerifyReader struct {
+	expectedHash []byte
+	hash         hash.Hash
+}
+
+func (h *hashVerifyReader) Read([]byte) (int, error) {
+	if bytes.Compare(h.hash.Sum(nil), h.expectedHash) != 0 {
+		return 0, errors.New("hashVerifyReader: digest mismatch")
+	}
+
+	return 0, io.EOF
+}
---
+++ hashverifyreader/reader_test.go
@@ -0,0 +1,44 @@
+package hashverifyreader
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/function61/gokit/assert"
+	"io/ioutil"
+	"testing"
+)
+
+func TestReaderGoodHash(t *testing.T) {
+	input := bytes.NewBufferString("The quick brown fox jumps over the lazy dog")
+
+	verifiedReader := New(
+		input,
+		sha256.New(),
+		hexToBytes("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592"))
+
+	out, err := ioutil.ReadAll(verifiedReader)
+	assert.True(t, err == nil)
+	assert.EqualString(t, string(out), "The quick brown fox jumps over the lazy dog")
+}
+
+func TestReaderBadHash(t *testing.T) {
+	input := bytes.NewBufferString("The quick brown fox jumps over the lazy dog")
+
+	thisHashWillNotMatch := New(
+		input,
+		sha256.New(),
+		hexToBytes("7885198ae80b48ebf6c0d0d6b8ce86169570d346ec0b2c43029d04b3e3763ec3"))
+
+	_, err := ioutil.ReadAll(thisHashWillNotMatch)
+	assert.EqualString(t, err.Error(), "hashVerifyReader: digest mismatch")
+}
+
+func hexToBytes(input string) []byte {
+	out, err := hex.DecodeString(input)
+	if err != nil {
+		panic(err)
+	}
+
+	return out
+}
