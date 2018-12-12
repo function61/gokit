6bc29177e495714dcaec6d522deb9d0c75f43cec
Joonas Loppi <joonas@joonas.fi>
2018-12-12 18:15:26 +0200

    dynversion: IsDevVersion()

--- dynversion/version.go
+++ dynversion/version.go
@@ -2,3 +2,7 @@ package dynversion
 
 // replaced dynamically at build time from function61/buildkit-golang/build-common.sh
 var Version = "dev"
+
+func IsDevVersion() bool {
+	return Version == "dev"
+}
