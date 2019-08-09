9a752cc0e4ae366fe9865c393942901425d3cee4
Joonas Loppi <joonas@joonas.fi>
2019-08-09 18:56:52 +0300

    systemdinstaller: testability, add RequireNetworkOnline

--- systemdinstaller/systemdinstaller.go
+++ systemdinstaller/systemdinstaller.go
@@ -3,61 +3,109 @@ package systemdinstaller
 
 import (
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-// FIXME(security): args are not shell escaped - DO NOT TAKE THIS FROM USER INPUT
-func InstallSystemdServiceFile(servicename string, args []string, description string) (string, error) {
-	unitfilePath := "/etc/systemd/system/" + servicename + ".service"
+type serviceFile struct {
+	servicename          string
+	args                 []string
+	description          string
+	requireNetworkOnline bool
+	selfAbsolutePath     string
+	err                  error // if error reading selfAbsolutePath
+}
 
+type optFn func(*serviceFile)
+
+func SystemdServiceFile(servicename string, description string, opts ...optFn) serviceFile {
+	selfAbsolutePath, err := filepath.Abs(os.Args[0])
+
+	sf := serviceFile{
+		servicename:      servicename,
+		description:      description,
+		selfAbsolutePath: selfAbsolutePath,
+		err:              err,
+	}
+
+	for _, opt := range opts {
+		opt(&sf)
+	}
+
+	return sf
+}
+
+func GetHints(sf serviceFile) string {
+	return strings.Join([]string{
+		"Wrote unit file to " + unitfilePath(sf),
+		"Run to enable on boot & to start now:",
+		"	$ systemctl enable " + sf.servicename,
+		"	$ systemctl start " + sf.servicename,
+		"	$ systemctl status " + sf.servicename,
+	}, "\n")
+}
+
+// FIXME(security): args are not shell escaped - DO NOT TAKE THIS FROM USER INPUT
+func Install(sf serviceFile) error {
+	if sf.err != nil {
+		return sf.err
+	}
+
+	if _, errStat := os.Stat(unitfilePath(sf)); errStat == nil || !os.IsNotExist(errStat) {
+		return errors.New("systemd service file already exists!")
+	}
+
+	if err := ioutil.WriteFile(unitfilePath(sf), []byte(serialize(sf)), 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func serialize(sf serviceFile) string {
+	lines := []string{}
+
+	l := func(line string) {
+		lines = append(lines, line)
+	}
+
+	l("[Unit]")
+	l("Description=" + sf.description)
+
+	// https://unix.stackexchange.com/a/126146
+	if sf.requireNetworkOnline {
+		l("Wants=network-online.target")
+		l("After=network-online.target")
+	}
+
+	l("")
+	l("[Install]")
+	l("WantedBy=multi-user.target")
+	l("")
+	l("[Service]")
+	l("ExecStart=" + strings.Join(append([]string{sf.selfAbsolutePath}, sf.args...), " "))
+	l("WorkingDirectory=" + filepath.Dir(sf.selfAbsolutePath))
+	l("Restart=always")
+	l("RestartSec=10s")
+	l("Environment=LOGGER_SUPPRESS_TIMESTAMPS=1")
 	// why LOGGER_SUPPRESS_TIMESTAMPS? journalctl adds its own timestamps to each line,
 	// so this is redundant data
-	unitTemplate := `[Unit]
-Description=%s
 
-[Install]
-WantedBy=multi-user.target
+	return strings.Join(lines, "\n") + "\n"
+}
 
-[Service]
-ExecStart=%s
-WorkingDirectory=%s
-Restart=always
-RestartSec=10s
-Environment=LOGGER_SUPPRESS_TIMESTAMPS=1
-`
+func unitfilePath(sf serviceFile) string {
+	return "/etc/systemd/system/" + sf.servicename + ".service"
+}
 
-	selfAbsolutePath, errAbs := filepath.Abs(os.Args[0])
-	if errAbs != nil {
-		return "", errAbs
+func Args(args ...string) optFn {
+	return func(sf *serviceFile) {
+		sf.args = args
 	}
+}
 
-	cmd := append([]string{selfAbsolutePath}, args...)
-
-	unitContent := fmt.Sprintf(
-		unitTemplate,
-		description,
-		strings.Join(cmd, " "),
-		filepath.Dir(selfAbsolutePath))
-
-	if _, errStat := os.Stat(unitfilePath); errStat == nil || !os.IsNotExist(errStat) {
-		return "", errors.New("systemd service file already exists!")
-	}
-
-	if err := ioutil.WriteFile(unitfilePath, []byte(unitContent), 0755); err != nil {
-		return "", err
-	}
-
-	hints := []string{
-		"Wrote unit file to " + unitfilePath,
-		"Run to enable on boot & to start now:",
-		"	$ systemctl enable " + servicename,
-		"	$ systemctl start " + servicename,
-		"	$ systemctl status " + servicename,
-	}
-
-	return strings.Join(hints, "\n"), nil
+func RequireNetworkOnline(sf *serviceFile) {
+	sf.requireNetworkOnline = true
 }
---
+++ systemdinstaller/systemdinstaller_test.go
@@ -0,0 +1,57 @@
+package systemdinstaller
+
+import (
+	"github.com/function61/gokit/assert"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	sf := SystemdServiceFile("testservice", "My cool service", Args("start"))
+	sf = fixForTest(sf)
+
+	assert.EqualString(t, serialize(sf), `[Unit]
+Description=My cool service
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+ExecStart=/home/dummy/testservice_amd64 start
+WorkingDirectory=/home/dummy
+Restart=always
+RestartSec=10s
+Environment=LOGGER_SUPPRESS_TIMESTAMPS=1
+`)
+
+	assert.EqualString(t, GetHints(sf), `Wrote unit file to /etc/systemd/system/testservice.service
+Run to enable on boot & to start now:
+	$ systemctl enable testservice
+	$ systemctl start testservice
+	$ systemctl status testservice`)
+}
+
+func TestRequireNetworkOnline(t *testing.T) {
+	sf := SystemdServiceFile("testservice", "My cool service", Args("start"), RequireNetworkOnline)
+	sf = fixForTest(sf)
+
+	assert.EqualString(t, serialize(sf), `[Unit]
+Description=My cool service
+Wants=network-online.target
+After=network-online.target
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+ExecStart=/home/dummy/testservice_amd64 start
+WorkingDirectory=/home/dummy
+Restart=always
+RestartSec=10s
+Environment=LOGGER_SUPPRESS_TIMESTAMPS=1
+`)
+}
+
+func fixForTest(sf serviceFile) serviceFile {
+	sf.selfAbsolutePath = "/home/dummy/testservice_amd64" // need to monkey patch this to get deterministic output
+	return sf
+}
