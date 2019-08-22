22b167aeaf3ffd75a618a11e0fc3c882472cde7c
Joonas Loppi <joonas@joonas.fi>
2019-08-22 11:55:50 +0300

    backoff: was broken past ~40 iterations, also add thread safety via mutex

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -2,25 +2,36 @@
 package backoff
 
 import (
+	"sync"
 	"time"
 )
 
 type Func func() time.Duration
 
+// given (100ms, 1s) => [0ms, 100ms, 200ms, 400ms, 800ms, 1s, 1s, 1s, ...]
 func ExponentialWithCappedMax(base time.Duration, max time.Duration) Func {
-	idx := -1
+	idx := uint(0)
+	idxMu := sync.Mutex{}
 
 	return func() time.Duration {
-		idx++
+		idxMu.Lock()
+		defer idxMu.Unlock()
 
+		var multiplier time.Duration
 		if idx == 0 {
-			return 0 * time.Millisecond
+			multiplier = 0
+		} else {
+			multiplier = 1 << (idx - 1)
 		}
 
-		sleepDuration := (1 << uint(idx-1)) * base
+		sleepDuration := multiplier * base
 
 		if sleepDuration > max {
 			sleepDuration = max
+		} else {
+			// if we'd just shift past max forever we'd overflow long before
+			// we get to iteration #64
+			idx++
 		}
 
 		return sleepDuration
--- backoff/backoff_test.go
+++ backoff/backoff_test.go
@@ -1,6 +1,7 @@
 package backoff
 
 import (
+	"fmt"
 	"github.com/function61/gokit/assert"
 	"testing"
 	"time"
@@ -9,11 +10,84 @@ import (
 func TestBackoff(t *testing.T) {
 	backoffDuration := ExponentialWithCappedMax(100*time.Millisecond, 1*time.Second)
 
-	assert.Assert(t, backoffDuration() == 0*time.Millisecond)
-	assert.Assert(t, backoffDuration() == 100*time.Millisecond)
-	assert.Assert(t, backoffDuration() == 200*time.Millisecond)
-	assert.Assert(t, backoffDuration() == 400*time.Millisecond)
-	assert.Assert(t, backoffDuration() == 800*time.Millisecond)
-	assert.Assert(t, backoffDuration() == 1000*time.Millisecond)
-	assert.Assert(t, backoffDuration() == 1000*time.Millisecond)
+	oneSec := 1000 * time.Millisecond
+
+	// why so many test case items? I failed so hard that after 40th iteration we started
+	// to converge on producing 0s...
+	expectations := []time.Duration{
+		0 * time.Millisecond,
+		100 * time.Millisecond,
+		200 * time.Millisecond,
+		400 * time.Millisecond,
+		800 * time.Millisecond,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec,
+		oneSec, // just to be sure, go past iteration 64
+		oneSec,
+		oneSec,
+		oneSec,
+	}
+
+	for idx, expectation := range expectations {
+		t.Run(fmt.Sprintf("Iteration %d", idx), func(t *testing.T) {
+			assert.Assert(t, backoffDuration() == expectation)
+		})
+	}
 }
