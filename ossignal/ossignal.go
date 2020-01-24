df1bf12a569ae75f2eecf81342f179f33c294e9e
Joonas Loppi <joonas@joonas.fi>
2020-01-24 17:45:09 +0200

    ossignal: InterruptOrTerminateBackgroundCtx()

--- ossignal/ossignal.go
+++ ossignal/ossignal.go
@@ -2,6 +2,9 @@
 package ossignal
 
 import (
+	"context"
+	"github.com/function61/gokit/logex"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,10 +14,21 @@ import (
 // "The SIGTERM signal is sent to a process to request its termination"
 // "SIGINT is nearly identical to SIGTERM"
 //     https://en.wikipedia.org/wiki/Signal_(IPC)
-
 func InterruptOrTerminate() <-chan os.Signal {
 	// need a buffered channel so a nonblocking select will catch this
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	return ch
+}
+
+// returns (background-derived) context that cancels on SIGINT or SIGTERM
+func InterruptOrTerminateBackgroundCtx(logger *log.Logger) context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		logex.Levels(logger).Info.Printf("got %s; stopping", <-InterruptOrTerminate())
+		cancel()
+	}()
+
+	return ctx
 }
