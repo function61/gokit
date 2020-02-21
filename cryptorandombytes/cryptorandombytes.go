f4f715f4fbb97f2449368a0d2e58c1e8333dd926
Joonas Loppi <joonas@joonas.fi>
2020-02-21 12:39:19 +0200

    cryptorandombytes: Base64UrlWithoutLeadingDash

--- cryptorandombytes/cryptorandombytes.go
+++ cryptorandombytes/cryptorandombytes.go
@@ -15,6 +15,19 @@ func Base64Url(bytesLen int) string {
 	return base64.RawURLEncoding.EncodeToString(randBytes(bytesLen))
 }
 
+// CLI arguments beginning with dash are problematic (which base64 URL variant can produce),
+// so we'll be nice guys and guarantee that the ID won't start with one.
+func Base64UrlWithoutLeadingDash(length int) string {
+	id := Base64Url(length)
+
+	if id[0] == '-' {
+		// try again. the odds should exponentially decrease for recursion level to increase
+		return Base64UrlWithoutLeadingDash(length)
+	}
+
+	return id
+}
+
 func randBytes(bytesLen int) []byte {
 	bytes := make([]byte, bytesLen)
 
--- cryptorandombytes/cryptorandombytes_test.go
+++ cryptorandombytes/cryptorandombytes_test.go
@@ -9,12 +9,15 @@ func TestHex(t *testing.T) {
 	assert.Assert(t, len(Hex(2)) == 4)
 	assert.Assert(t, len(Hex(4)) == 8)
 	assert.Assert(t, len(Hex(8)) == 16)
-
 }
 
-func TestBase64(t *testing.T) {
+func TestBase64Url(t *testing.T) {
 	// base64 has 33.33 % wasted space
 	assert.Assert(t, len(Base64Url(3)) == 4)
 	assert.Assert(t, len(Base64Url(6)) == 8)
 	assert.Assert(t, len(Base64Url(7)) == 10)
+}
+
+func TestBase64UrlWithoutLeadingDash(t *testing.T) {
+	assert.Assert(t, len(Base64UrlWithoutLeadingDash(3)) == 4)
 }
