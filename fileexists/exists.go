1720ecfda58e7eba09fdb04014a4a0432606bb3e
Joonas Loppi <joonas@joonas.fi>
2020-04-01 19:00:56 +0300

    fileexists: ExistsNoLinkFollow()

--- fileexists/exists.go
+++ fileexists/exists.go
@@ -6,8 +6,17 @@ import (
 )
 
 // https://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
+// follows symlinks
 func Exists(path string) (bool, error) {
-	_, err := os.Stat(path)
+	return exists(path, os.Stat)
+}
+
+func ExistsNoLinkFollow(path string) (bool, error) {
+	return exists(path, os.Lstat)
+}
+
+func exists(path string, statter func(string) (os.FileInfo, error)) (bool, error) {
+	_, err := statter(path)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return false, nil
