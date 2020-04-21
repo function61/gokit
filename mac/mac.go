385d8b5eda78109e27eb30e463d0f72f00f823c3
Joonas Loppi <joonas@joonas.fi>
2020-04-21 18:16:08 +0300

    mac: use crypto/hmac and sha256, increase entropy (by switching from hex to base64)

--- mac/mac.go
+++ mac/mac.go
@@ -1,37 +1,43 @@
-// Message Authentication Code: make sure a message is not tampered with while in-transit
-// by a possibly malicious party.
+// Message Authentication Code - to make sure a message is not tampered with while
+// in-transit by a malicious party. Small usability wrapper around crypto/hmac
 package mac
 
 import (
-	"crypto/sha1"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"errors"
-	"fmt"
 )
 
-var ErrMacValidationFailed = errors.New("mac validation failed")
+var ErrMacValidationFailed = errors.New("MAC validation failed")
 
 type Mac struct {
-	key     string
-	message string
+	key     []byte
+	message []byte
 }
 
 func New(key string, message string) *Mac {
-	return &Mac{key: key, message: message}
+	return &Mac{key: []byte(key), message: []byte(message)}
 }
 
 func (m *Mac) Authenticate(givenMac string) error {
-	if m.Sign() != givenMac {
+	// safe from timing attacks
+	if !hmac.Equal([]byte(givenMac), []byte(m.Sign())) {
 		return ErrMacValidationFailed
 	}
 
 	return nil
 }
 
+// NOTE: entropy is lowered for prettier URLs
 func (m *Mac) Sign() string {
-	keyAndMessageCombined := []byte(m.key + ":" + m.message)
+	hmacHash := hmac.New(sha256.New, m.key)
+	if _, err := hmacHash.Write(m.message); err != nil {
+		panic(err)
+	}
 
-	sumHex := fmt.Sprintf("%x", sha1.Sum(keyAndMessageCombined))
+	sumB64 := base64.RawURLEncoding.EncodeToString(hmacHash.Sum(nil))
 
-	// cap to length of 16 for prettier URLs. we still have 64 bits of entropy
-	return sumHex[0:16]
+	// cap length for prettier URLs. we still have 96 bits of entropy
+	return sumB64[0:16]
 }
--- mac/mac_test.go
+++ mac/mac_test.go
@@ -16,20 +16,20 @@ func TestAuthenticate(t *testing.T) {
 
 	signature := New(key1, msg).Sign()
 
-	assert.EqualString(t, signature, "f2a78249534b6b01")
+	assert.EqualString(t, signature, "bo4dqebLiFXPTpqv")
 
 	assert.Assert(t, New(key1, msg).Authenticate(signature) == nil)
 	assert.Assert(t, New(key1, msg).Authenticate("wrong signature") == ErrMacValidationFailed)
 }
 
 func TestDifferentMessagesProduceDifferentSignatures(t *testing.T) {
-	assert.EqualString(t, New(key1, "msg A").Sign(), "93945caa95ab362f")
-	assert.EqualString(t, New(key1, "msg B").Sign(), "00070486a22d08b4")
+	assert.EqualString(t, New(key1, "msg A").Sign(), "eq5Wd5Qh2cCgonpj")
+	assert.EqualString(t, New(key1, "msg B").Sign(), "HOc4MPpRdV4ytr4r")
 }
 
 func TestDifferentKeysProduceDifferentSignatures(t *testing.T) {
 	msg := "message to authenticate"
 
-	assert.EqualString(t, New(key1, msg).Sign(), "765b53a2f01b28ea")
-	assert.EqualString(t, New(key2, msg).Sign(), "19207bd1bc992f97")
+	assert.EqualString(t, New(key1, msg).Sign(), "PwBQuOJaF34tjmv2")
+	assert.EqualString(t, New(key2, msg).Sign(), "q8PkJSBswgeTrFcX")
 }
