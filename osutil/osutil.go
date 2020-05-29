2438152e3de4d543230c5fc0443a998d3c8c5197
Joonas Loppi <joonas@joonas.fi>
2020-05-29 14:51:08 +0300

    osutil: rename InterruptOrTerminateBackgroundCtx() -> CancelOnInterruptOrTerminate()
    also, remove InterruptOrTerminate()

--- osutil/osutil.go
+++ osutil/osutil.go
@@ -12,20 +12,18 @@ import (
 	"github.com/function61/gokit/logex"
 )
 
-// "The SIGINT signal is sent to a process by its controlling terminal when a user wishes to interrupt the process"
-// "The SIGTERM signal is sent to a process to request its termination"
-// "SIGINT is nearly identical to SIGTERM"
-//     https://en.wikipedia.org/wiki/Signal_(IPC)
-func InterruptOrTerminate() <-chan os.Signal {
-	// need a buffered channel so a nonblocking select will catch this
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	return ch
-}
-
-// returns (background-derived) context that cancels on SIGINT or SIGTERM
-func InterruptOrTerminateBackgroundCtx(logger *log.Logger) context.Context {
+// makes (background-derived) context that cancels on SIGINT ("Ctrl + c") or SIGTERM.
+func CancelOnInterruptOrTerminate(logger *log.Logger) context.Context {
 	ctx, cancel := context.WithCancel(context.Background())
+
+	// need a buffered channel because the sending side is non-blocking
+	ch := make(chan os.Signal, 1)
+
+	// "The SIGINT signal is sent to a process by its controlling terminal when a user wishes to interrupt the process"
+	// "The SIGTERM signal is sent to a process to request its termination"
+	// "SIGINT is nearly identical to SIGTERM"
+	//     https://en.wikipedia.org/wiki/Signal_(IPC)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		logex.Levels(logger).Info.Printf("got %s; stopping", <-InterruptOrTerminate())
