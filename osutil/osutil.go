523e0bdb2287c731c751ccc36317dcf11f0a85b6
Joonas Loppi <joonas@joonas.fi>
2020-05-29 14:49:09 +0300

    osutil: add ExitIfError()

--- osutil/osutil.go
+++ osutil/osutil.go
@@ -3,6 +3,7 @@ package osutil
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -32,4 +33,14 @@ func InterruptOrTerminateBackgroundCtx(logger *log.Logger) context.Context {
 	}()
 
 	return ctx
+}
+
+// if "err" is non-nil, exits with exit code 1 and prints the given error to stderr
+func ExitIfError(err error) {
+	// abstraction seems silly, but I've written this so many times, and some of the times
+	// wrong (stdout instead of stderr), that maybe it's better to write this properly once
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
