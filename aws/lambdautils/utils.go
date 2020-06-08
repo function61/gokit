12235c68c38bb61a01a48d46a9fe7b866ca52b01
Joonas Loppi <joonas@joonas.fi>
2020-06-08 13:59:53 +0300

    lambdautils: add NoPayloadAdapter

--- aws/lambdautils/utils.go
+++ aws/lambdautils/utils.go
@@ -2,6 +2,7 @@
 package lambdautils
 
 import (
+	"context"
 	"os"
 )
 
@@ -9,4 +10,13 @@ import (
 func InLambda() bool {
 	// https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html
 	return os.Getenv("LAMBDA_TASK_ROOT") != ""
+}
+
+// simple function as a Lambda handler by discarding input payload and returning empty
+// payload. this is available in AWS's lambda.Start(handler interface{}) but it's not typesafe
+// (runtime error if fn sig doesn't match)
+type NoPayloadAdapter func(context.Context) error
+
+func (s NoPayloadAdapter) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
+	return []byte{}, s(ctx)
 }
