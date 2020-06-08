b92d3f0b0c48c633d1c8f23640e220d81addd4a5
Joonas Loppi <joonas@joonas.fi>
2020-06-08 11:53:34 +0300

    s3facade: improve

--- aws/s3facade/s3facade.go
+++ aws/s3facade/s3facade.go
@@ -1,30 +1,75 @@
 // Because AWS SDK has so much ceremony..
 package s3facade
 
-// why the facade? there's so many things happening to set up the client that I always
-// struggle finding the correct AWS APIs, when usually all I want to do is the same..
-
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/function61/gokit/envvar"
 )
 
-func Client(akid string, secret string, regionId string) (*s3.S3, error) {
+// this abstraction so we support immediate validation of credentials, unlike AWS's API
+type CredentialsObtainer func() (*credentials.Credentials, error)
+
+// giving nil "credentials" is supported, if you want the automatic procedure
+func Bucket(
+	bucket string,
+	obtainCredentials CredentialsObtainer,
+	region string,
+) (*BucketContext, error) {
+	config := aws.NewConfig().WithRegion(region)
+
+	if obtainCredentials != nil {
+		credentials, err := obtainCredentials()
+		if err != nil {
+			return nil, err
+		}
+
+		config.WithCredentials(credentials)
+	}
+
+	return BucketWithConfig(bucket, config)
+}
+
+func BucketWithConfig(bucket string, config *aws.Config) (*BucketContext, error) {
 	awsSession, err := session.NewSession()
 	if err != nil {
 		return nil, err
 	}
 
-	manualCredential := credentials.NewStaticCredentials(
-		akid,
-		secret,
-		"")
+	return &BucketContext{
+		Name: &bucket,
+		S3: s3.New(
+			awsSession,
+			config),
+	}, nil
+}
 
-	s3Client := s3.New(
-		awsSession,
-		aws.NewConfig().WithCredentials(manualCredential).WithRegion(regionId))
+type BucketContext struct {
+	Name *string // pointer because all S3 operations take it as such
+	S3   *s3.S3
+}
 
-	return s3Client, nil
+// pretty much same as AWS's credentials.NewEnvCredentials(), except this fails fast instead
+// of when S3 operations are first called
+func CredentialsFromEnv() (*credentials.Credentials, error) {
+	accessKeyId, err := envvar.Required("AWS_ACCESS_KEY_ID")
+	if err != nil {
+		return nil, err
+	}
+
+	accessKeySecret, err := envvar.Required("AWS_SECRET_ACCESS_KEY")
+	if err != nil {
+		return nil, err
+	}
+
+	return credentials.NewStaticCredentials(accessKeyId, accessKeySecret, ""), nil
+}
+
+// adapter for making from AWS's Credentials
+func Credentials(creds *credentials.Credentials) CredentialsObtainer {
+	return func() (*credentials.Credentials, error) {
+		return creds, nil
+	}
 }
