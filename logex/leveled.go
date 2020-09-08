0d6671f9a1a4dcd4add5d4218d3fd9cb4606c43d
Joonas Loppi <joonas@joonas.fi>
2020-09-08 12:42:16 +0300

    logex.Leveled: improve documentation

--- logex/leveled.go
+++ logex/leveled.go
@@ -4,10 +4,13 @@ import (
 	"log"
 )
 
+// purposefully minimal # of log levels. I kind of agree with this post ..:
+//   https://dave.cheney.net/2015/11/05/lets-talk-about-logging
+// .. with the exception, that Error levels are justified
 type Leveled struct {
-	Debug *log.Logger
-	Info  *log.Logger
-	Error *log.Logger
+	Debug *log.Logger // usually hidden, maybe noisy, but can be enabled for debugging deep problems
+	Info  *log.Logger // everything else not fitting in debug or error
+	Error *log.Logger // something exceptional operations usually should look at: like I/O (network/disk) failed, service call failed, security problem etc.
 
 	// convenience for if some component needs raw (= non-leveled) Logger
 	Original *log.Logger
