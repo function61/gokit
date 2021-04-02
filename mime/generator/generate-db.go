5970e1a4535dc081debb75163b507015afa79627
Joonas Loppi <joonas@joonas.fi>
2021-04-02 15:57:46 +0300

    forgot that gokit itself uses deprecated fn

--- mime/generator/generate-db.go
+++ mime/generator/generate-db.go
@@ -26,7 +26,7 @@ func logic() error {
 	if _, err := ezhttp.Get(
 		context.Background(),
 		"https://raw.githubusercontent.com/jshttp/mime-db/master/db.json",
-		ezhttp.RespondsJson(&remoteDb, false),
+		ezhttp.RespondsJsonDisallowUnknownFields(&remoteDb),
 	); err != nil {
 		return err
 	}
