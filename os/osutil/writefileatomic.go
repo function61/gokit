6e65352704c3039728631b884facaf58bfb80389
Joonas Loppi <joonas@joonas.fi>
2021-06-28 15:26:23 +0300

    osutil: WriteFileAtomic(): support permissions, owners and timestamps

--- os/osutil/writefileatomic.go
+++ os/osutil/writefileatomic.go
@@ -5,7 +5,9 @@ package osutil
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
+	"time"
 )
 
 // - creates a <filename>.part file to write to
@@ -13,7 +15,12 @@ import (
 // - rename temp filename to filename if everything went OK
 //
 // If encountering any errors, remove the partial file
-func WriteFileAtomic(filename string, produce func(io.Writer) error) error {
+func WriteFileAtomic(filename string, produce func(io.Writer) error, options ...WriteFileOption) error {
+	opts := writeFileOptions{}
+	for _, option := range options {
+		option(&opts)
+	}
+
 	tempFilename := filename + ".part"
 
 	tempFile, err := os.Create(tempFilename)
@@ -42,8 +49,26 @@ func WriteFileAtomic(filename string, produce func(io.Writer) error) error {
 		return cleanup(err, false)
 	}
 
+	if opts.mode != nil {
+		if err := tempFile.Chmod(*opts.mode); err != nil {
+			return err
+		}
+	}
+
+	if opts.uid != nil {
+		if err := tempFile.Chown(*opts.uid, *opts.gid); err != nil {
+			return err
+		}
+	}
+
 	if err := tempFile.Close(); err != nil {
 		return cleanup(err, true) // do not try closing again
+	}
+
+	if opts.atime != nil { // unfortunately this has to be done after Close()
+		if err := os.Chtimes(tempFilename, *opts.atime, *opts.mtime); err != nil {
+			return err
+		}
 	}
 
 	if err := os.Rename(tempFilename, filename); err != nil {
@@ -63,4 +88,35 @@ func (writeNopCloser) Close() error { return nil }
 // the provided Writer w.
 func newWriteNopCloser(w io.Writer) io.WriteCloser {
 	return writeNopCloser{w}
+}
+
+type writeFileOptions struct {
+	mode  *fs.FileMode
+	uid   *int
+	gid   *int
+	atime *time.Time
+	mtime *time.Time
+}
+
+type WriteFileOption func(opt *writeFileOptions)
+
+func WriteFileMode(mode fs.FileMode) WriteFileOption {
+	return func(opt *writeFileOptions) {
+		opt.mode = &mode
+	}
+}
+
+func WriteFileChown(uid int, gid int) WriteFileOption {
+	return func(opt *writeFileOptions) {
+		opt.uid = &uid
+		opt.gid = &gid
+	}
+}
+
+// warning: ctime is ignored for now
+func WriteFileTimes(atime time.Time, mtime time.Time, ctime time.Time) WriteFileOption {
+	return func(opt *writeFileOptions) {
+		opt.atime = &atime
+		opt.mtime = &mtime
+	}
 }
