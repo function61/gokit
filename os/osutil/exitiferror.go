a771c27739d498e57d9fcaa3ec5f8a1a4c60a7ba
Joonas Loppi <joonas+code@joonas.fi>
2022-05-16 16:38:39 +0300

    osutil: ExitIfError(): make error messages stand out more

--- os/osutil/exitiferror.go
+++ os/osutil/exitiferror.go
@@ -6,11 +6,18 @@ import (
 )
 
 // if "err" is non-nil, exits with exit code 1 and prints the given error to stderr
+//
+// intended to be ran once from program's entrypoint / main function
+//
+// this abstraction seems silly, but I've written this so many times, and some of the times
+// wrong (stdout instead of stderr), that maybe it's better to write this properly once
 func ExitIfError(err error) {
-	// abstraction seems silly, but I've written this so many times, and some of the times
-	// wrong (stdout instead of stderr), that maybe it's better to write this properly once
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	if err == nil { // happy case :)
+		return
 	}
+
+	// non-technical folk more often have trouble recognizing error condition from message, so
+	// better make it STAND OUT that the following is an error message
+	fmt.Fprintf(os.Stderr, "✗ ERROR: %s\n", err.Error())
+	os.Exit(1)
 }
