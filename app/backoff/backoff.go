496bf23876b5cf4eef5db36bdc4876aec7314c2d
Joonas Loppi <joonas+code@joonas.fi>
2023-03-15 11:34:35 +0200

    backoff: add `Fixed()`

--- app/backoff/backoff.go
+++ app/backoff/backoff.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+// when an operation is reasonably expected to fail at times and retry attempts should be done,
+// this function should be called after each attempt to get the new duration before the next attempt should be tried.
+//
+// usual implementations are expected to return different duration for each call, like [0ms, 100ms, 200ms, 400ms, ...]
 type Func func() time.Duration
 
 // given (100ms, 1s) => [0ms, 100ms, 200ms, 400ms, 800ms, 1s, 1s, 1s, ...]
@@ -36,4 +40,9 @@ func ExponentialWithCappedMax(base time.Duration, max time.Duration) Func {
 
 		return sleepDuration
 	}
+}
+
+// uses a fixed duration for backoff (always returns e.g. 1s, 1s, ...)
+func Fixed(dur time.Duration) Func {
+	return func() time.Duration { return dur }
 }
