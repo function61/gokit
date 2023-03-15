f861136c2305fa77e263ce76a72fb5e26b6a616e
Joonas Loppi <joonas+code@joonas.fi>
2023-03-15 11:35:16 +0200

    retry: add `IgnoreErrorsWithin()`, `ErrorsToLogger()`

--- app/retry/retry.go
+++ app/retry/retry.go
@@ -4,6 +4,7 @@ package retry
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/function61/gokit/app/backoff"
@@ -46,4 +47,28 @@ func Retry(
 // 0ms, 100 ms, 200 ms, 400 ms, 800 ms, 1000 ms, 1000 ms, ...
 func DefaultBackoff() backoff.Func {
 	return backoff.ExponentialWithCappedMax(100*time.Millisecond, 1*time.Second)
+}
+
+// creates `Retry()` compatible error handler that only starts reacting to errors after a specific initial duration
+func IgnoreErrorsWithin(expectErrorsWithin time.Duration, handleError func(error)) func(error) {
+	retryGroupStarted := time.Now()
+
+	return func(err error) {
+		// during this time window attempts are expected to fail and thus we should not confuse the user.
+		if time.Since(retryGroupStarted) <= expectErrorsWithin {
+			return
+		}
+
+		// now in this time window we have passes the expected failures and the failures have likely
+		// become unexpected. inform ther user.
+
+		handleError(err)
+	}
+}
+
+// creates `Retry()` compatible error handler function that just pushes errors to a logger
+func ErrorsToLogger(logger *log.Logger) func(error) {
+	return func(err error) {
+		logger.Println(err.Error())
+	}
 }
