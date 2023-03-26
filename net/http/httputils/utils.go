71740f8ca1a6dd1c518b6b9fe764c9f4e7e25ed5
Joonas Loppi <joonas+code@joonas.fi>
2023-03-26 15:11:11 +0300

    httputils: add `DefaultReadHeaderTimeout`

--- net/http/httputils/utils.go
+++ net/http/httputils/utils.go
@@ -5,6 +5,14 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"time"
+)
+
+var (
+	// this has to be given always when making a server to mitigate slowrosis attack:
+	//   https://en.wikipedia.org/wiki/Slowloris_(computer_security)
+	// value same as nginx: https://www.oreilly.com/library/view/nginx-http-server/9781788623551/0b1ce6c8-4863-433c-bb70-bf9aa565654c.xhtml
+	DefaultReadHeaderTimeout = 60 * time.Second
 )
 
 func Error(w http.ResponseWriter, statusCode int) {
