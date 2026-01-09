bda704f0ea3d3d5e0441bd6d126ff7a8b890f0ca
Joonas Loppi <joonas+code@joonas.fi>
2026-01-09 16:15:22 +0200

    `udocker`: endpoint URLs for different API versions

--- app/udocker/endpoints.go
+++ app/udocker/endpoints.go
@@ -1,16 +1,32 @@
 package udocker
 
-const (
-	ListContainersEndpoint = "/v1.24/containers/json"
-	TasksEndpoint          = "/v1.24/tasks?filters=%7B%22desired-state%22%3A%5B%22running%22%5D%7D"
-	ServicesEndpoint       = "/v1.24/services"
-	NodesEndpoint          = "/v1.24/nodes"
+var (
+	DefaultVersion = EndpointVersion("1.24")
 )
 
-func ContainerInspectEndpoint(containerId string) string {
-	return "/v1.24/containers/" + containerId + "/json"
+type VersionedEndpoints struct {
+	baseURL string
 }
 
-func NetworkInspectEndpoint(networkId string) string {
-	return "/v1.24/networks/" + networkId
+// creates "factory" with which you can make URLs like `/v1.44/containers/json`
+func EndpointVersion(version string) VersionedEndpoints {
+	return VersionedEndpoints{"/v" + version}
+}
+
+func (v VersionedEndpoints) ListContainersEndpoint() string { return v.baseURL + "/containers/json" }
+
+func (v VersionedEndpoints) TasksEndpoint() string {
+	return v.baseURL + "/tasks?filters=%7B%22desired-state%22%3A%5B%22running%22%5D%7D"
+}
+
+func (v VersionedEndpoints) ServicesEndpoint() string { return v.baseURL + "/services" }
+
+func (v VersionedEndpoints) NodesEndpoint() string { return v.baseURL + "/nodes" }
+
+func (v VersionedEndpoints) ContainerInspectEndpoint(containerId string) string {
+	return v.baseURL + "/containers/" + containerId + "/json"
+}
+
+func (v VersionedEndpoints) NetworkInspectEndpoint(networkId string) string {
+	return v.baseURL + "/networks/" + networkId
 }
